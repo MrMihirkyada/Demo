55751b4cbe751d106f0fe428e01fae14c251fd44
Mihirkyada1111 <130635496+Mihirkyada1111@users.noreply.github.com>
2023-12-13 11:15:35 +0530

    code added

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.demo
+
+import android.accessibilityservice.GestureDescription
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.InitializationStatus
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+    }
+
+
+}
